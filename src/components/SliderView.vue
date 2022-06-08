e6394f7e35d96bdd05995fabe95b4b2e6ba8a3e6
ValentinSaraiva-MMI <95092193+ValentinSaraiva-MMI@users.noreply.github.com>
2022-06-09 01:08:37 +0200

    moon cake

--- src/components/SliderView.vue
+++ src/components/SliderView.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="my-5 pl-1 py-5 flex flex-row gap-5 overflow-x-scroll">
+    <PetitCarteTerrain
+      class="flex-none scale-110"
+      nom="Terrain de Tennis Silhouette"
+      note="5"
+      distance="1.2 km"
+      image="public\silhouette.jpg"
+    />
 
-        <PetitCarteTerrain class="flex-none scale-110"
-          nom="Terrain de Basket Châteaudun"
-          note="3.8"
-          distance="210 m"
-          image="public\basket1.jpg"
-        />
+    <PetitCarteTerrain
+      class="flex-none scale-110"
+      nom="Terrain de Basket Châteaudun"
+      note="3.8"
+      distance="210 m"
+      image="public\terrain_chateaudun.jpg"
+    />
 
-        <PetitCarteTerrain class="flex-none scale-110"
-          nom="Skatepark  Harley Davidson"
-          note="4.7"
-          distance="4.3km"
-          image="public\basket1.jpg"
-        />
-
-        <PetitCarteTerrain class="flex-none scale-110"
-          nom="Skatepark  Harley Davidson"
-          note="4.7"
-          distance="4.3km"
-          image="public\basket1.jpg"
-        />
+    <PetitCarteTerrain
+      class="flex-none scale-110"
+      nom="Stade Mougnot"
+      note="4.0"
+      distance="10.1 km"
+      image="public/images/terrain3.jpg"
+    />
   </div>
 </template>
 
@@ -29,7 +31,7 @@
 
 
 <script>
-import PetitCarteTerrain from "./PetitCarteTerrain.vue"
+import PetitCarteTerrain from "./PetitCarteTerrain.vue";
 
 export default {
   name: "slider",
