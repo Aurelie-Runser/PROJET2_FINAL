9dacec7146d2326a58b7e1c9c83e8f51197309a9
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-06-08 16:43:18 +0200

    creation de la page UpdateProfil

--- src/views/Profil/UpdateView.vue
+++ src/views/Profil/UpdateView.vue
@@ -6,45 +6,66 @@
       </p>
     </div>
 
-    <form>
+    <h1 class="mx-5 my-5 text-xl font-arial font-bold">Modification profil</h1>
 
-        <div class="w-full aspect-video md:aspect-[9/3] rounded-2xl overflow-hidden">
-            <img class="object-cover" alt="banniere" :src="banniereData"/>
+    <form @submit.prevent="updateProfil">
 
-            <input type="file" class="block my-12 w-full h-max" ref="file" id="file"
-                                @change="previewImage" >
-        </div>
+        <div class="m-5">
+            <h2 class="mx-5 my-3 text-lg font-arial font-bold">Bannière</h2>
 
-        <div class="w-max">
-            <div class="relative rounded-full overflow-hidden w-24 h-24">
-            <img class="h-full object-cover" :src="avatarData" alt="avatar" />
+            <div class="mx-auto w-11/12 aspect-video md:aspect-[9/3] rounded-2xl overflow-hidden">
+                <img class="w-full object-cover" alt="banniere" :src="banniereData"/>
             </div>
-            <input type="file" class="block my-12 w-full h-max" ref="file" id="file"
-                                @change="previewImage" >
+            <input type="file"
+                    class="mx-3 my-5" 
+                    ref="file"
+                    id="file"
+                    @change="previewBanniere">
         </div>
     
+        <div class="m-5">
+            <h2 class="mx-5 my-5 text-lg font-arial font-bold">Photo de profil</h2>
+            
+            <div class="rounded-full overflow-hidden mx-auto w-32 h-32">
+                <img class="h-full object-cover" :src="avatarData" alt="avatar" />
+            </div>
+            <input type="file"
+                    class="mx-3 my-5"
+                    ref="file"
+                    id="file"
+                    @change="previewAvatar">
+        </div>
+    
+        <div class="m-5">
+            <h2 class="mx-5 my-5 text-lg font-arial font-bold">Prénom, Nom et Description</h2>
 
-        <h2 class="text-center mt-14 mb-5 text-3xl font-medium">{{Profils.prenom}} {{Profils.Nom}}</h2>
+            <input class="border-2 border-green-550 my-3 px-3 py-2"
+                    placeholder="Votre prénom"
+                    v-model="Profils.prenom"
+                    required /> 
 
-        <input class="block my-12 w-full h-max" placeholder="Votre prénom"
-                                v-model="Profils.prenom"
-                                required /> 
+            <input class="border-2 border-green-550 my-3 px-3 py-2"
+                    placeholder="Votre Nom"
+                    v-model="Profils.Nom"
+                    required /> 
+        
+            <textarea
+            name="DescriptionProfil"
+            id="DescriptionProfil"
+            cols="30"
+            rows="5"
+            required
+            placeholder="Ajouter description ✏️"
+            autocomplete="off"
+            class="border-2 border-green-550 my-3 px-3 py-2"
+            v-model="Profils.description">
+            </textarea>
+        </div>
 
-        <input class="block my-12 w-full h-max" placeholder="Votre prénom"
-                                v-model="Profils.Nom"
-                                required /> 
-
-        <textarea
-        name="DescriptionProfil"
-        id="DescriptionProfil"
-        cols="35"
-        rows="5"
-        required
-        placeholder="Ajouter description ✏️"
-        autocomplete="off"
-        class="border-b border-b-gray-500 mx-5 my-5 py-2"
-        v-model="Profils.desciption">
-        </textarea>
+        <button class="block mx-auto my-5 p-3 bg-green-450 rounded-xl shadow-lg font-arial font-bold text-xl text-white text-center"
+                   type="submit">
+                Modifier
+            </button>
 
     </form>
 
@@ -189,6 +210,7 @@ export default {
         if(this.refProfil.exists()){
             this.Profils = this.refProfil.data();
             this.avatarActuelle = this.Profils.avatar;
+            this.banniereActuelle = this.Profils.banniere;
         }
         else{
             this.console.log("Groupe inexistant");
@@ -202,26 +224,68 @@ export default {
         .catch((error) =>{
             console.log('erreur downloadUrl', error);
         })
+        const spaceRef2 = ref(storage, 'banniere/'+this.Profils.banniere);
+        getDownloadURL(spaceRef2)
+        .then((url) => {
+            this.banniereData = url;
+        })
+        .catch((error) =>{
+            console.log('erreur downloadUrl', error);
+        })
     },
 
-    previewImage: function(event) {
+    previewAvatar: function(event) {
         this.file = this.$refs.file.files[0];
-            // Récupérer le nom du fichier pour la photo du participant
         this.Profils.avatar = this.file.name;
-            // Si cette fonction s'exécute, c'est que l'image est modifiée 
         this.avatarModifie = true;
-            // Reference du fichier à prévisualiser
         var input = event.target;
-            // On s'assure que l'on a au moins un fichier à lire
         if (input.files && input.files[0]) {
             var reader = new FileReader();
             reader.onload = (e) => {
                 this.avatarData = e.target.result;
             }
-               // Demarrage du reader pour la transformer en data URL (format base 64) 
             reader.readAsDataURL(input.files[0]);        
         }
     },
+
+    previewBanniere: function(event) {
+        this.file = this.$refs.file.files[0];
+        this.Profils.banniere = this.file.name;
+        this.banniereModifie = true;
+        var input = event.target;
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = (e) => {
+                this.banniereData = e.target.result;
+            }
+            reader.readAsDataURL(input.files[0]);        
+        }
+    },
+
+
+    async updateProfil(){
+            if(this.avatarModifie){
+                const storage = getStorage();
+                let docRef = ref(storage, 'avatar/'+this.avatarActuelle);
+                deleteObject(docRef);
+                docRef = ref(storage, 'avatar/'+this.Profils.avatar);
+                await uploadString(docRef, this.avatarData, 'data_url').then((snapshot) => {
+                    console.log('Uploaded a base64 string', this.Profils.avatar);                
+                });                   
+            }
+            if(this.banniereModifie){
+                const storage = getStorage();
+                let docRef = ref(storage, 'banniere/'+this.banniereActuelle);
+                deleteObject(docRef);
+                docRef = ref(storage, 'banniere/'+this.Profils.banniere);
+                await uploadString(docRef, this.banniereData, 'data_url').then((snapshot) => {
+                    console.log('Uploaded a base64 string', this.Profils.banniere);                
+                });                   
+            }
+            const firestore = getFirestore();
+            await updateDoc(doc(firestore, "Profils", this.$route.params.id), this.Profils);
+            this.$router.push('/profil');       
+        }
   },
 
 };
